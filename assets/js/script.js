3f431ddac7b2b81ee7f7e076296d2c1ecc78a25c
Your Name <you@example.com>
2026-07-19 15:15:12 +0500

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -133,27 +133,15 @@ for (let i = 0; i < formInputs.length; i++) {
 
   });
 }
+// =============================
+// Auto Open Contact Sidebar
+// =============================
 
+window.addEventListener("load", function () {
 
+  setTimeout(() => {
+    sidebar.classList.add("active");
+  }, 1.5 * 1000); // 2.5 seconds delay
 
-// page navigation variables
-const navigationLinks = document.querySelectorAll("[data-nav-link]");
-const pages = document.querySelectorAll("[data-page]");
+});
 
-// add event to all nav link
-for (let i = 0; i < navigationLinks.length; i++) {
-  navigationLinks[i].addEventListener("click", function () {
-
-    for (let i = 0; i < pages.length; i++) {
-      if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
-        pages[i].classList.add("active");
-        navigationLinks[i].classList.add("active");
-        window.scrollTo(0, 0);
-      } else {
-        pages[i].classList.remove("active");
-        navigationLinks[i].classList.remove("active");
-      }
-    }
-
-  });
-}
